c587a80a455458d2bfa47f6427ab73d1d3cf32f6
Ona Solani <onasolani@gmail.com>
2020-12-14 20:58:07 +0100

    Antiguo portfolio

--- assets/js/navbar-gsap.js
+++ assets/js/navbar-gsap.js
@@ -15,3 +15,40 @@ $(".close-btn").click(function() {
     $("#resize").toggleClass("active");
 });
 
+// nav ends here
+
+// nav animation
+
+// TweenMax.from("#brand", 1, {
+//     delay: 0.4,
+//     y: 10,
+//     opacity: 0,
+//     ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".media-title", 1, {
+//     delay: 0.6,
+//     y: 10,
+//     opacity: 0,
+//     ease: Expo.easeInOut
+// })
+
+// TweenMax.from(".copyright, .hero-projects", 1, {
+//     delay: 0.8,
+//     y: 10,
+//     opacity: 0,
+//     ease: Expo.easeInOut
+// })
+
+// TweenMax.staggerFrom("#menu li a, .media li a", 1, {
+//     delay: 0.4,
+//     y: 10,
+//     opacity: 0,
+//     ease: Expo.easeInOut
+// }, 0.1);
+
+
+
+// nav animation ends
+
+
