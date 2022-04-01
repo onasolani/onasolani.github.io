520d942f96aa48b918439f48b2328f7e89cb010e
Ona <ona.solani@zondax.ch>
2022-04-01 16:17:21 +0200

    new portfolio

--- assets/js/navbar-gsap.js
+++ assets/js/navbar-gsap.js
@@ -3,15 +3,18 @@
 $("#toggle").click(function() {
     $(this).toggleClass('on');
     $("#resize").toggleClass("active");
+    $("#scroll").toggleClass("scroll");
 });
 
 $("#resize ul li a").click(function() {
     $(this).toggleClass('on');
     $("#resize").toggleClass("active");
+    $("#scroll").toggleClass("scroll");
 });
 
 $(".close-btn").click(function() {
     $(this).toggleClass('on');
     $("#resize").toggleClass("active");
+    $("#scroll").toggleClass("scroll");
 });
 
